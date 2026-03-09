5988bb533ede54092f571fc093f845b7d15d80ff
chorom-corder <chorom2@gmail.com>
2026-03-10 01:41:46 +0900

    update

--- public/editor.js
+++ public/editor.js
@@ -269,7 +269,7 @@ document.addEventListener("DOMContentLoaded", () => {
   updateCount()
   updateLockUI()
   updateTimer()
-  //tryFullscreen()
+  tryFullscreen()
  document.addEventListener("click", tryFullscreen, { once: true })
 })
 
